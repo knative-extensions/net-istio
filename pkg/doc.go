58a7ab470edc123267b6e1e18e3a5d76b4b9a003
Matt Moore <mattmoor@vmware.com>
2020-03-02 17:51:52 -0800

    Use the published contributing links. (#9)
    * Use the published contributing links.
    
    This is consistent with Serving and everywhere else, and will hopefully be more stable.
    
    * Add a dummy pkg.
    
    * Fix another link in DEVELOPMENT.md
    
    * Update codegen

--- pkg/doc.go
+++ pkg/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Kubernetes Authors.
+Copyright 2020 The Knative Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package validation contains generic api type validation functions.
-package validation // import "k8s.io/apimachinery/pkg/api/validation"
+// Package pkg is a placeholder to make e2e tests pass until real code is added.
+package pkg
