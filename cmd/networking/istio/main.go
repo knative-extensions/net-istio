knative-extensions/net-certmanager

Fix component names. (#4735)

main.go
@@ -24,6 +24,6 @@ import (
 )
 
 func main() {
-	sharedmain.Main("controller-certificate-cert-manager",
+	sharedmain.Main("certcontroller",
 		certificate.NewController)
 }

knative-extensions/net-istio

cmd/networking/istio/main.go
@@ -24,6 +24,6 @@ import (
 )
 
 func main() {
-	sharedmain.Main("controller-ingress-istio",
+	sharedmain.Main("istiocontroller",
 		clusteringress.NewController)
 }