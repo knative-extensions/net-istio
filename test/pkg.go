1959c7d458bf24a12a0e5987f4627a49775bf592
Nghia Tran <nghia@google.com>
2020-03-07 16:13:28 -0800

    Try to pull conformance/ingress tests into vendor/ (#31)

---
+++ test/pkg.go
@@ -0,0 +1,22 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package test
+
+import (
+	// Import the conformance ingress tests so that they show up in
+	// knative.dev/net-istio/vendor/
+	_ "knative.dev/serving/test/conformance/ingress"
+)
