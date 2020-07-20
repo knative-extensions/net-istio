343650322b2a6f7f18f0b14c1f39aa10cb5b7ecb
Matt Moore <mattmoor@vmware.com>
2020-07-20 14:39:02 -0700

    Enable chaos testing on net-istio

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -62,6 +62,8 @@ function test_setup() {
   echo ">> Bringing up net-istio Ingress Controller"
   ko apply -f config/ || return 1
 
+  scale_controlplane networking-istio istio-webhook
+  
   # Wait for pods to be running.
   echo ">> Waiting for Istio components to be running..."
   wait_until_pods_running istio-system || return 1
@@ -70,6 +72,17 @@ function test_setup() {
 
   # Wait for static IP to be through
   wait_until_service_has_external_http_address istio-system istio-ingressgateway
+}
+
+function scale_controlplane() {
+  for deployment in "$@"; do
+    # Make sure all pods run in leader-elected mode.
+    kubectl -n knative-serving scale deployment "$deployment" --replicas=0 || failed=1
+    # Give it time to kill the pods.
+    sleep 5
+    # Scale up components for HA tests
+    kubectl -n knative-serving scale deployment "$deployment" --replicas=2 || failed=1
+  done
 }
 
 # Add function call to trap
