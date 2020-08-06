7156cf79506ee7246f583b22c83490fbd0838f8f
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-08-07 01:27:24 +0900

    Update install-istio.sh to support version and yaml URL

--- third_party/istio-1.5.7/install-istio.sh
+++ third_party/istio-1.5.7/install-istio.sh
@@ -15,30 +15,42 @@
 # limitations under the License.
 
 # Download and unpack Istio
-ISTIO_VERSION=1.5.7
+if [[ ${ISTIO_VERSION} == "stable" ]]; then
+  ISTIO_VERSION="1.5.7"
+elif [[ ${ISTIO_VERSION} == "latest" ]]; then
+  ISTIO_VERSION="1.6.4"
+else
+  ISTIO_VERSION="${ISTIO_VERSION:-"1.5.7"}"
+fi
+
 ISTIO_TARBALL=istio-${ISTIO_VERSION}-linux.tar.gz
 DOWNLOAD_URL=https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/${ISTIO_TARBALL}
+
+if [[ $# != 1 ]]; then
+  echo "usage: $0 [ISTIO_OPERATOR_YAML_PATH|ISTIO_OPERATOR_YAML_URL]"; exit 1
+fi
+
+if [[ $1 =~ ^https?:// ]]; then
+  ISTIO_MANIFEST=$(curl -s -k $1)
+else
+  ISTIO_MANIFEST=$(cat $1)
+fi
+
+SUBCOMMAND="manifest apply"
+if [[ ${ISTIO_VERSION} =~ ^1\.6 ]]; then
+  SUBCOMMAND="install"
+fi
 
 wget --no-check-certificate $DOWNLOAD_URL
 if [ $? != 0 ]; then
   echo "Failed to download Istio package"
   exit 1
 fi
-tar xzf ${ISTIO_TARBALL} 
+tar xzf ${ISTIO_TARBALL}
 
 # Install Istio
-./istio-${ISTIO_VERSION}/bin/istioctl manifest apply -f "$(dirname $0)/$1"
+echo "${ISTIO_MANIFEST}" | ./istio-${ISTIO_VERSION}/bin/istioctl ${SUBCOMMAND} -f -
 
 # Clean up
 rm -rf istio-${ISTIO_VERSION}
 rm ${ISTIO_TARBALL}
-
-## Add in the `istio-system` namespace to reduce number of commands.
-#patch istio-crds.yaml namespace.yaml.patch
-#patch istio-ci-mesh.yaml namespace.yaml.patch
-#patch istio-ci-no-mesh.yaml namespace.yaml.patch
-#patch istio-minimal.yaml namespace.yaml.patch
-#
-## Increase termination drain duration seconds.
-#patch -l istio-ci-mesh.yaml drain-seconds.yaml.patch
-
