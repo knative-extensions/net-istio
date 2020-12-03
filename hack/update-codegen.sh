c624b0b5962600914ba7bf8bd7c79dc269e888e4
Arghya Sadhu <arghya88@gmail.com>
2020-12-03 18:42:50 +0530

    integrate the gopath hacks
    Signed-off-by: Arghya Sadhu <arghya88@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,40 +18,40 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export GO111MODULE=on
+source $(dirname $0)/../vendor/knative.dev/hack/codegen-library.sh
+
 # If we run with -mod=vendor here, then generate-groups.sh looks for vendor files in the wrong place.
 export GOFLAGS=-mod=
 
-if [ -z "${GOPATH:-}" ]; then
-  export GOPATH=$(go env GOPATH)
-fi
+echo "=== Update Codegen for $MODULE_NAME"
 
-REPO_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
-
-KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT}; ls -d -1 ./vendor/knative.dev/pkg 2>/dev/null || echo ../pkg)}
+group "Knative Codegen"
 
 # Knative Injection (for istio)
-chmod +x ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh
 ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
   knative.dev/net-istio/pkg/client/istio istio.io/client-go/pkg/apis \
   "networking:v1alpha3" \
-  --go-header-file ${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt
+  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
+
+group "Kubernetes Codegen"
 
 # Generate our own client for istio (otherwise injection won't work)
-chmod +x ${CODEGEN_PKG}/generate-groups.sh
 ${CODEGEN_PKG}/generate-groups.sh "client,informer,lister" \
   knative.dev/net-istio/pkg/client/istio istio.io/client-go/pkg/apis \
   "networking:v1alpha3" \
-  --go-header-file ${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt
+  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
+
+group "Deepcopy Gen"
 
 # Depends on generate-groups.sh to install bin/deepcopy-gen
 ${GOPATH}/bin/deepcopy-gen \
   -O zz_generated.deepcopy \
-  --go-header-file ${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt \
+  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt \
   -i knative.dev/net-istio/pkg/reconciler/ingress/config \
   -i knative.dev/net-istio/pkg/defaults
 
+group "Update deps post-codegen"
+
 # Make sure our dependencies are up-to-date
-${REPO_ROOT}/hack/update-deps.sh
+${REPO_ROOT_DIR}/hack/update-deps.sh
 
