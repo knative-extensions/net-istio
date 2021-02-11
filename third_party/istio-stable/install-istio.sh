77e94f686200f618346f77f05a1d26274daf8c85
Jean-Rémy Bancel <bancel@google.com>
2021-02-11 11:54:34 -0800

    Temporarily disable mTLS STRICT in mesh mode.

--- third_party/istio-stable/install-istio.sh
+++ third_party/istio-stable/install-istio.sh
@@ -23,7 +23,8 @@ trap cleanup_istio EXIT
 # Install Istio
 ${ISTIO_DIR}/bin/istioctl install -f "$(dirname $0)/$1"
 
+# Temporarily disable this (https://github.com/knative-sandbox/net-istio/issues/503)
 # Enable mTLS STRICT in mesh mode
-if [[ $MESH -eq 1 ]]; then
-  kubectl apply -f "$(dirname $0)/extra/global-mtls.yaml"
-fi
+# if [[ $MESH -eq 1 ]]; then
+#   kubectl apply -f "$(dirname $0)/extra/global-mtls.yaml"
+# fi
