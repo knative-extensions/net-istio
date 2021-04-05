a87da9a9904dad0d28eddfaa109d7998a90949b8
Dave Protasowski <dprotaso@gmail.com>
2021-04-04 22:50:47 -0400

    precompute istio manifests

--- third_party/istio-latest/generate-manifests.sh
+++ third_party/istio-latest/generate-manifests.sh
@@ -1,10 +1,12 @@
-# Copyright 2021 The Knative Authors
+#!/usr/bin/env bash
+
+# Copyright 2020 The Knative Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: istio-system
----
+source "$(dirname $0)/../library.sh"
+
+generate "1.9.1" "$(dirname $0)" \
+      --set values.pilot.env.PILOT_ENABLE_STATUS=true \
+      --set values.global.istiod.enableAnalysis=true \
