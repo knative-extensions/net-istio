1369deb459fd6e9d8101a72c21b2bfeab0d41588
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-08-17 10:35:16 +0900

    Bump Istio stable and latest to 1.10.3 and 1.11.0 (#722)

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -65,13 +65,8 @@ group "Update istio generated manifests"
 # want our presubmit checks to fail because a diff occurred
 # ${REPO_ROOT_DIR}/third_party/istio-head/generate_manifests.sh
 
-# We don't update stable - manifest generate (1.8.4) seems to have
-# bugs where it outputs duplicate resources in a non-deterministic order.
-# So re-running the command will produce a diff.
-#
-# Renable this once we reach a version # where that is fixed (ie. 1.9 seems fine)
-# ${REPO_ROOT_DIR}/third_party/istio-stable/generate-manifests.sh
 
-# Update latest cause the istioctl version seems to generate
+# Update stable and latest cause the istioctl version seems to generate
 # reproducible yaml. So let's check for unexpected diffs
+${REPO_ROOT_DIR}/third_party/istio-stable/generate-manifests.sh
 ${REPO_ROOT_DIR}/third_party/istio-latest/generate-manifests.sh
