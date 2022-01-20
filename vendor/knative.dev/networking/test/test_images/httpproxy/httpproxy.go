b44067da2782d3bb36e06ff15f7231f41101d6e7
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-01-20 10:45:52 +0900

    Try to revert proxy image

--- vendor/knative.dev/networking/test/test_images/httpproxy/httpproxy.go
+++ vendor/knative.dev/networking/test/test_images/httpproxy/httpproxy.go
@@ -4,8 +4,7 @@ Copyright 2018 The Knative Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -40,65 +39,50 @@ const (
 	defaultPort = "8080"
 )
 
-func main() {
-	flag.Parse()
-	log.Print("HTTP Proxy app started.")
+var (
+	httpProxy *httputil.ReverseProxy
+)
 
-	// Fetch port from env or use default if not provided.
-	port := os.Getenv(portEnv)
-	if port == "" {
-		port = defaultPort
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("HTTP proxy received a request.")
+	// Reverse proxy does not automatically reset the Host header.
+	// We need to manually reset it.
+	r.Host = getTargetHostEnv()
+	httpProxy.ServeHTTP(w, r)
+}
 
-	// Fetch target host from env or fail if not provided.
-	targetHost := os.Getenv(targetHostEnv)
-	if targetHost == "" {
-		log.Fatalf("No value for env var %q provided.", targetHostEnv)
+func getPort() string {
+	value := os.Getenv(portEnv)
+	if value == "" {
+		return defaultPort
 	}
+	return value
+}
 
-	routedHost := targetHost
-	// gateway is an optional value. It is used only when resolvable domain is not set
-	// for external access test, as services like sslip.io may be flaky.
-	// ref: https://github.com/knative/serving/issues/5389
-	gateway := os.Getenv(gatewayHostEnv)
-	if gateway != "" {
-		routedHost = gateway
+func getTargetHostEnv() string {
+	value := os.Getenv(targetHostEnv)
+	if value == "" {
+		log.Fatalf("No env %v provided.", targetHostEnv)
 	}
+	return value
+}
 
-	target := &url.URL{
-		Scheme: "http",
-		Host:   routedHost,
+func initialHTTPProxy(proxyURL string) *httputil.ReverseProxy {
+	target, err := url.Parse(proxyURL)
+	if err != nil {
+		log.Fatal("Failed to parse url ", proxyURL)
 	}
-	log.Print("target is ", target)
 	proxy := httputil.NewSingleHostReverseProxy(target)
-	proxy.Transport = newDNSCachingTransport()
 	proxy.ErrorHandler = func(w http.ResponseWriter, req *http.Request, err error) {
 		log.Print("error reverse proxying request: ", err)
 		http.Error(w, err.Error(), http.StatusBadGateway)
 	}
-
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("HTTP proxy received a request.")
-		// Reverse proxy does not automatically reset the Host header.
-		// We need to manually reset it.
-		r.Host = targetHost
-		proxy.ServeHTTP(w, r)
-	})
-	// Handle forwarding requests which uses "K-Network-Hash" header.
-	handler = network.NewProbeHandler(handler).ServeHTTP
-
-	address := ":" + port
-	log.Print("Listening on address: ", address)
-	test.ListenAndServeGracefully(address, handler)
+	return proxy
 }
 
-// newDNSCachingTransport caches DNS lookups locally to avoid issues like
-// dial tcp: lookup ingress-conformance-0-visibility-split-ijpryqql.serving-tests.svc.c1550209590.local on 10.96.0.10:53: dial udp 10.96.0.10:53: operation was canceled
-func newDNSCachingTransport() http.RoundTripper {
+func newDNSCachingDialer() func(context.Context, string, string) (net.Conn, error) {
 	resolver := &dnscache.Resolver{}
-
-	t := http.DefaultTransport.(*http.Transport).Clone()
-	t.DialContext = func(ctx context.Context, network string, addr string) (conn net.Conn, err error) {
+	return func(ctx context.Context, network string, addr string) (conn net.Conn, err error) {
 		host, port, err := net.SplitHostPort(addr)
 		if err != nil {
 			return nil, err
@@ -116,6 +100,38 @@ func newDNSCachingTransport() http.RoundTripper {
 		}
 		return
 	}
+}
 
-	return t
+func newDNSCachingTransport() http.RoundTripper {
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.DisableKeepAlives = false
+	transport.DialContext = newDNSCachingDialer()
+	transport.MaxIdleConns = 1000
+	transport.MaxIdleConnsPerHost = 100
+	return transport
+}
+
+func main() {
+	flag.Parse()
+	log.Print("HTTP Proxy app started.")
+
+	targetHost := getTargetHostEnv()
+	port := getPort()
+
+	// Gateway is an optional value. It is used only when resolvable domain is not set
+	// for external access test, as xip.io is flaky.
+	// ref: https://github.com/knative/serving/issues/5389
+	gateway := os.Getenv(gatewayHostEnv)
+	if gateway != "" {
+		targetHost = gateway
+	}
+	targetURL := "http://" + targetHost
+	log.Print("target is " + targetURL)
+	httpProxy = initialHTTPProxy(targetURL)
+	httpProxy.Transport = newDNSCachingTransport()
+	address := ":" + port
+	log.Print("Listening on address: ", address)
+	// Handle forwarding requests which uses "K-Network-Hash" header.
+	probeHandler := network.NewProbeHandler(http.HandlerFunc(handler)).ServeHTTP
+	test.ListenAndServeGracefully(address, probeHandler)
 }
