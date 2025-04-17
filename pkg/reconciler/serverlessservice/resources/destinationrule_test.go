13e8ecc269e456cb65ec9cc09236ccc3c2ded5aa
Dave Protasowski <dprotaso@gmail.com>
2025-04-17 09:54:01 -0400

    add test coverage

---
+++ pkg/reconciler/serverlessservice/resources/destinationrule_test.go
@@ -0,0 +1,74 @@
+package resources
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	istiov1beta1 "istio.io/api/networking/v1beta1"
+	istiov1beta1clientset "istio.io/client-go/pkg/apis/networking/v1beta1"
+	"knative.dev/networking/pkg/apis/networking/v1alpha1"
+	"knative.dev/pkg/kmeta"
+	pkgnetwork "knative.dev/pkg/network"
+)
+
+// Constants defined in virtualservice_test.go can be used here as well
+// if they are in the same package. Let's redefine them for clarity just in case,
+// though in a real scenario they might be in a shared test helper.
+const (
+	testDRNamespace = "test-dr-namespace"
+	testDRSksName   = "test-dr-sks"
+	testDRSvcName   = "test-dr-svc"
+)
+
+func TestMakeDestinationRule(t *testing.T) {
+	sks := &v1alpha1.ServerlessService{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testDRNamespace,
+			Name:      testDRSksName,
+			UID:       "test-dr-uid",
+		},
+		Status: v1alpha1.ServerlessServiceStatus{
+			PrivateServiceName: testDRSvcName,
+		},
+	}
+
+	expectedHost := pkgnetwork.GetServiceHostname(testDRSvcName, testDRNamespace)
+	expected := &istiov1beta1clientset.DestinationRule{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            testDRSvcName,
+			Namespace:       testDRNamespace,
+			OwnerReferences: []metav1.OwnerReference{*kmeta.NewControllerRef(sks)},
+		},
+		Spec: istiov1beta1.DestinationRule{
+			Host: expectedHost,
+			Subsets: []*istiov1beta1.Subset{{
+				Name: subsetNormal,
+				TrafficPolicy: &istiov1beta1.TrafficPolicy{
+					LoadBalancer: &istiov1beta1.LoadBalancerSettings{
+						LbPolicy: &istiov1beta1.LoadBalancerSettings_Simple{
+							Simple: istiov1beta1.LoadBalancerSettings_LEAST_REQUEST,
+						},
+					},
+				},
+			}, {
+				Name: subsetDirect,
+				TrafficPolicy: &istiov1beta1.TrafficPolicy{
+					LoadBalancer: &istiov1beta1.LoadBalancerSettings{
+						LbPolicy: &istiov1beta1.LoadBalancerSettings_Simple{
+							Simple: istiov1beta1.LoadBalancerSettings_PASSTHROUGH,
+						},
+					},
+				},
+			}},
+		},
+	}
+
+	got := MakeDestinationRule(sks)
+
+	if diff := cmp.Diff(expected, got, protocmp.Transform()); diff != "" {
+		t.Errorf("MakeDestinationRule (-want, +got):\n%s", diff)
+	}
+}
---
+++ pkg/reconciler/serverlessservice/resources/virtualservice_test.go
@@ -0,0 +1,79 @@
+package resources
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+	istiov1beta1 "istio.io/api/networking/v1beta1"
+	istiov1beta1clientset "istio.io/client-go/pkg/apis/networking/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/networking/pkg/apis/networking/v1alpha1"
+	"knative.dev/networking/pkg/http/header"
+	"knative.dev/pkg/kmeta"
+	pkgnetwork "knative.dev/pkg/network"
+)
+
+const (
+	testNamespace = "test-namespace"
+	testSksName   = "test-sks"
+	testSvcName   = "test-svc"
+)
+
+func TestMakeVirtualService(t *testing.T) {
+	sks := &v1alpha1.ServerlessService{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testNamespace,
+			Name:      testSksName,
+			UID:       "test-uid",
+		},
+		Spec: v1alpha1.ServerlessServiceSpec{
+			Mode: v1alpha1.SKSOperationModeProxy,
+		},
+		Status: v1alpha1.ServerlessServiceStatus{
+			PrivateServiceName: testSvcName,
+		},
+	}
+
+	expectedHost := pkgnetwork.GetServiceHostname(testSvcName, testNamespace)
+	expected := &istiov1beta1clientset.VirtualService{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            testSvcName,
+			Namespace:       testNamespace,
+			OwnerReferences: []metav1.OwnerReference{*kmeta.NewControllerRef(sks)},
+		},
+		Spec: istiov1beta1.VirtualService{
+			Hosts: []string{expectedHost},
+			Http: []*istiov1beta1.HTTPRoute{{
+				Match: []*istiov1beta1.HTTPMatchRequest{{
+					Headers: map[string]*istiov1beta1.StringMatch{
+						header.PassthroughLoadbalancingKey: {
+							MatchType: &istiov1beta1.StringMatch_Exact{
+								Exact: "true",
+							},
+						},
+					},
+				}},
+				Route: []*istiov1beta1.HTTPRouteDestination{{
+					Destination: &istiov1beta1.Destination{
+						Host:   expectedHost,
+						Subset: subsetDirect,
+					},
+				}},
+			}, {
+				Route: []*istiov1beta1.HTTPRouteDestination{{
+					Destination: &istiov1beta1.Destination{
+						Host:   expectedHost,
+						Subset: subsetNormal,
+					},
+				}},
+			}},
+		},
+	}
+
+	got := MakeVirtualService(sks)
+
+	if diff := cmp.Diff(expected, got, protocmp.Transform()); diff != "" {
+		t.Errorf("MakeVirtualService (-want, +got):\n%s", diff)
+	}
+}
